soimy/libgpiod-android

commit 452e4bac0c85addf7126ddcd039b323f3beba8f5

bindings: cxx: fix event timestamp calculation for 32bit
Use appropriate C++ chrono library functions to convert the event
timestamp from a struct timespec to ::std::chrono::nanoseconds to
ensure correct conversion independent of platform.

Fixes: 8078a4a2ad90 ("bindings: implement C++ bindings")
Reported-by: Florian Evers <florian-evers@gmx.de>
Signed-off-by: Kent Gibson <warthog618@gmail.com>
Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>

bindings/cxx/line.cpp
@@ -168,8 +168,9 @@ line_event line::event_read(void) const
 	else if (event_buf.event_type == GPIOD_LINE_EVENT_FALLING_EDGE)
 		event.event_type = line_event::FALLING_EDGE;
 
-	event.timestamp = ::std::chrono::nanoseconds(
-				event_buf.ts.tv_nsec + (event_buf.ts.tv_sec * 1000000000));
+	event.timestamp = ::std::chrono::duration_cast<::std::chrono::nanoseconds>(
+				::std::chrono::seconds(event.ts.tv_sec)) +
+				::std::chrono::nanoseconds(event.ts.tv_nsec);
 
 	event.source = *this;
 

commit ef344377f73e94fbef4dbb23ff8a64f575b26e13

bindings: cxx: fix build error
Due to a bad backport the build now fails with the following error:

line.cpp: In member function ‘gpiod::line_event gpiod::line::event_read() const’:
line.cpp:172:34: error: ‘struct gpiod::line_event’ has no member named ‘ts’
     ::std::chrono::seconds(event.ts.tv_sec)) +
                                  ^~
line.cpp:173:38: error: ‘struct gpiod::line_event’ has no member named ‘ts’
     ::std::chrono::nanoseconds(event.ts.tv_nsec);
                                      ^~
make[4]: *** [Makefile:557: libgpiodcxx_la-line.lo] Error 1

This change fixes it.

Fixes: 452e4bac0c85 ("bindings: cxx: fix event timestamp calculation for 32bit")
Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>

bindings/cxx/line.cpp
@@ -169,8 +169,8 @@ line_event line::event_read(void) const
 		event.event_type = line_event::FALLING_EDGE;
 
 	event.timestamp = ::std::chrono::duration_cast<::std::chrono::nanoseconds>(
-				::std::chrono::seconds(event.ts.tv_sec)) +
-				::std::chrono::nanoseconds(event.ts.tv_nsec);
+				::std::chrono::seconds(event_buf.ts.tv_sec)) +
+				::std::chrono::nanoseconds(event_buf.ts.tv_nsec);
 
 	event.source = *this;
 